55eb28f013ac277f75bc7b3c42c1b0888ed5d0ff
Rayan <rayanthefirst@gmail.com>
2023-09-26 13:10:18 -0400

    Fixed featured page

--- src/components/Home/Featured.js
+++ src/components/Home/Featured.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "../Projects/ProjectCards"
-import stocktrading from "../../Assets/Projects/stock-trading.png"
-import backtesting from "../../Assets/Projects/backtesting.png"
+import FeaturedProjectCard from "./FeaturedProjectCard";
+import stocktrading from "../../Assets/Projects/stock-trading.png";
+import backtesting from "../../Assets/Projects/backtesting.png";
+import DMZ_Winner from "../../Assets/Projects/DMZ_Winner.PNG";
+
+
 
 function Featured() {
   return (
@@ -12,22 +15,26 @@ function Featured() {
       <Container>
       <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
-            <ProjectCard
+            <FeaturedProjectCard
               imgPath={stocktrading}
-              isBlog={false}
               title="Autonomous Stock Trading Systems"
               description="Created quantitative strategies, implemented them on Interactive Brokers, and deployed them on GCP"   
             />
           </Col>
 
           <Col md={4} className="project-card">
-            <ProjectCard
+            <FeaturedProjectCard
               imgPath={backtesting}
-              isBlog={false}
               title="Strategy Back-tester"
               description="Designed back-tester that reflects quantitative strategies on Interactive Brokers historical data with 90% accuracy"
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <FeaturedProjectCard
+              imgPath={DMZ_Winner}
+              title="NearMe"
+              description="NearMe is an innovative app that uses geolocation to connect post-secondary students based on their interests, promoting in-person interactions"
             />
           </Col>
         </Row>
