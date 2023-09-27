c430cfa21305a33da524e6ca3608c4dc533bff7c
Rayan <rayanthefirst@gmail.com>
2023-09-27 12:45:48 -0400

    added email to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import { MdEmail } from 'react-icons/md';
 
 function Footer() {
   let date = new Date();
@@ -10,20 +11,21 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Rayan Ahmad | Portfolio</h3>
+          <h3></h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons"  style={{textAlign: "center"}}>
-            <li className="social-icons">
+          <li className="social-icons">
               <a
-                href="https://github.com/rayanthefirst"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
+                  href="mailto:r29ahmad@uwaterloo.ca"
+                  style={{ color: "white" }}
+                  target="_blank" 
+                  rel="noopener noreferrer"
               >
-                <AiFillGithub />
+                  <MdEmail />
               </a>
             </li>
+           
             <li className="social-icons">
               <a
                 href="https://linkedin.com/in/rayan-ahmad29"
@@ -33,12 +35,24 @@ function Footer() {
               >
                 <FaLinkedinIn />
               </a>
+              </li>
+              <li className="social-icons">
+              <a
+                href="https://github.com/rayanthefirst"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
             </li>
+            
+            
           </ul>
         </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year}</h3>
-        </Col>
+        {/* <Col md="4" className="footer-copywright">
+          <h3>Portfolio</h3>
+        </Col> */}
         
       </Row>
     </Container>
